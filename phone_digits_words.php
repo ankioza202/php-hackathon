ae79b046da50776b9b943fd1efbf75aa96f4f7f0
Admin <admin@Admins-MacBook-Pro.local>
2019-08-21 21:28:35 -0600

    Print all possible words from phone digits

---
+++ phone_digits_words.php
@@ -0,0 +1,26 @@
+<?php
+
+# Print all possible letter from phone Digits Dialup
+function printWordRecursion($input,$curr,$output,$number){
+
+	$dialPadLetters = array("", "", "abc", "def", "ghi", "jkl",  
+                    "mno", "pqrs", "tuv", "wxyz");
+
+	$inputArr=str_split($input);
+	for($i=0;$i<strlen($dialPadLetters[$inputArr[$curr]]);$i++){
+		array_push($output,$dialPadLetters[$inputArr[$curr]][$i]);
+		printWordRecursion($input,$curr+1,$output,$number);
+		array_pop($output);
+	}
+
+	if($curr==$number){
+		$out = implode("", $output);
+		print($out."\n");
+	}
+
+}
+
+$inputNum="234";
+printWordRecursion($inputNum,0,[],strlen($inputNum));
+
+?>
